e237e43d976b903492592581ac52772679515bf6
yamizi <salah.ghamizi@gmail.com>
2021-03-30 11:55:15 +0200

    Welcome page

---
+++ src/pages/TabHome.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { IonSlides, IonSlide, IonContent } from '@ionic/react';
+
+import './TabHome.css';
+
+
+// Optional parameters to pass to the swiper instance.
+// See http://idangero.us/swiper/api/ for valid options.
+const slideOpts = {
+  initialSlide: 0,
+  speed: 400
+};
+
+const TabHome: React.FC = () => {
+  return (
+    <IonContent>
+    <IonSlides pager={true} options={slideOpts}>
+      <IonSlide>
+        <h1>Slide 1</h1>
+      </IonSlide>
+      <IonSlide>
+        <h1>Slide 2</h1>
+      </IonSlide>
+      <IonSlide>
+        <h1>Slide 3</h1>
+      </IonSlide>
+    </IonSlides>
+  </IonContent>
+  );
+};
+
+export default TabHome;
