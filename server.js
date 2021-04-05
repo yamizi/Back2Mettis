cc71262658ee5cd29c80118a9ea290bec9a601b3
yamizi <salah.ghamizi@gmail.com>
2021-04-05 14:03:59 +0200

    Updated static paths

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build', 'assets')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
