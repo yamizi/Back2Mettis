5cded40978227b351520e01243eba15a6585a77b
yamizi <salah.ghamizi@gmail.com>
2021-05-20 10:15:40 +0200

    New mission creation form

--- src/components/MissionComponent.tsx
+++ src/components/MissionComponent.tsx
@@ -5,10 +5,10 @@ import {setScene} from "../store/actions/sceneActions";
 import {connect} from "react-redux";
 import {withRouter} from "react-router";
 import {
-    IonContent, IonFab,
-    IonFabButton,
+    IonContent, IonGrid,
+    IonCol,
     IonHeader,
-    IonIcon,
+    IonRow,
     IonItem,
     IonLabel,
     IonList,
@@ -20,7 +20,7 @@ import {
 } from "@ionic/react";
 
 import {addOutline, closeOutline, checkmarkOutline } from "ionicons/icons";
-import { IonModal, IonButton, IonCheckbox } from '@ionic/react';
+import { IonModal, IonButton, IonCheckbox, IonFab, IonIcon, IonFabButton  } from '@ionic/react';
 
 
 
@@ -35,7 +35,8 @@ class MissionComponent extends Component<MapProps> {
             showModal: false,
             new_scenes:[],
             new_buttons:[],
-            new_game:{type:undefined, correctAnswer:""}
+            new_game:{type:undefined, correctAnswer:"", question:""},
+            new_name:"",
         };
 
         console.log(props.novel.scenes)
@@ -53,7 +54,13 @@ class MissionComponent extends Component<MapProps> {
     };
 
     setShowModal = (visible:boolean, save:boolean) => {
-        if(!save){
+
+
+        if(save) {
+            console.log(this.state)
+            return
+        }
+        else{
             this.setState({"new_scenes":[{"updatedImage":"","text":""}]})
         }
         this.setState({"showModal":visible})
@@ -61,6 +68,44 @@ class MissionComponent extends Component<MapProps> {
 
     setText = (label:string, txt:string) => {
 
+        switch(label){
+            case "new_name":{
+                this.setState({"new_name":txt})
+                break;
+            }
+            case "updated_image":{
+                let scenes = this.state.new_scenes
+                let indices = txt.split("###")
+                scenes[indices[0]].updatedImage = indices[1]
+                this.setState({"new_scenes":scenes})
+                break;
+            }
+            case "text":{
+                let scenes = this.state.new_scenes
+                let indices = txt.split("###")
+                scenes[indices[0]].text = indices[1]
+                this.setState({"new_scenes":scenes})
+                break;
+            }
+            case "qrcode_answer":{
+                let game = this.state.new_game
+                game.correctAnswer = txt
+                this.setState({"new_game":game})
+                break;
+            }
+            case "riddle_question":{
+                let game = this.state.new_game
+                game.question = txt
+                this.setState({"new_game":game})
+                break;
+            }
+            case "riddle_answer":{
+                let game = this.state.new_game
+                game.correctAnswer = txt
+                this.setState({"new_game":game})
+                break;
+            }
+        }
     };
 
     setButtonsChecked= (value:boolean) => {
@@ -71,6 +116,24 @@ class MissionComponent extends Component<MapProps> {
             this.setState({"new_buttons":[]})
         }
 
+    };
+
+    setQRCodeChecked = (value:boolean) => {
+        if (value){
+            this.setState({"new_game":{type:"qrcode", correctAnswer:""}})
+        }
+        else{
+            this.setState({"new_game":{type:undefined, correctAnswer:""}})
+        }
+    };
+
+    setRiddleChecked = (value:boolean) => {
+        if (value){
+            this.setState({"new_game":{type:"input", correctAnswer:""}})
+        }
+        else{
+            this.setState({"new_game":{type:undefined, correctAnswer:""}})
+        }
     };
 
     handleSceneClic = (scene:SceneType) => {
@@ -106,7 +169,7 @@ class MissionComponent extends Component<MapProps> {
 
                         <IonItemDivider>Titre de la mission</IonItemDivider>
                         <IonItem>
-                            <IonInput value={""} placeholder="Texte ici" onIonChange={e => this.setText("new_name",e.detail.value!)}></IonInput>
+                            <IonInput placeholder="Texte ici" onIonChange={e => this.setText("new_name",e.detail.value!)}></IonInput>
                           </IonItem>
 
 
@@ -114,10 +177,10 @@ class MissionComponent extends Component<MapProps> {
                              <div>
                                  <IonItemDivider>Scène {index+1}</IonItemDivider>
                                  <IonItem>
-                                    <IonInput value={""} placeholder="Image d'arrière plan (url du site web)" onIonChange={e => this.setText("updatedImage",e.detail.value!)}></IonInput>
+                                    <IonInput placeholder="Image d'arrière plan (url du site web)" onIonChange={e => this.setText("updated_image",index+"###"+e.detail.value!)}></IonInput>
                                   </IonItem>
                                 <IonItem>
-                                  <IonTextarea placeholder="Texte de la scène"  value={""} onIonChange={e => this.setText("text",e.detail.value!)}></IonTextarea>
+                                  <IonTextarea placeholder="Texte de la scène"  onIonChange={e => this.setText("text",index+"###"+e.detail.value!)}></IonTextarea>
                                 </IonItem>
                              </div>
                          )}
@@ -128,19 +191,47 @@ class MissionComponent extends Component<MapProps> {
                             <IonCheckbox checked={this.state.new_buttons.length >0} onIonChange={e => this.setButtonsChecked(e.detail.checked)} />
                           </IonItem>
 
+                        {
+                        this.state.new_buttons.length >0 &&
+                        <IonItem>
+                                <IonGrid>
+
+                                  <IonRow>
+                                    <IonCol size="6">ion-col size="6"</IonCol>
+                                    <IonCol>ion-col</IonCol>
+                                    <IonCol>ion-col</IonCol>
+                                  </IonRow>
+                                </IonGrid>
+                        </IonItem>
+                        }
                         <IonItem>
                             <IonLabel>Scanner un QR Code</IonLabel>
-                            <IonCheckbox checked={this.state.new_game.type=="qrcode"} onIonChange={e => this.setButtonsChecked(e.detail.checked)} />
-                          </IonItem>
+                            <IonCheckbox checked={this.state.new_game.type=="qrcode"} onIonChange={e => this.setQRCodeChecked(e.detail.checked)} />
+                        </IonItem>
+                        {
+                        this.state.new_game.type=="qrcode" &&
+                        <IonItem>
+                            <IonInput value={""} placeholder="Message du QR Code" onIonChange={e => this.setText("qrcode_answer",e.detail.value!)}></IonInput>
+                        </IonItem>
+                        }
 
                         <IonItem>
                             <IonLabel>Répondre à une question</IonLabel>
-                            <IonCheckbox checked={this.state.new_game.type=="input"} onIonChange={e => this.setButtonsChecked(e.detail.checked)} />
-                          </IonItem>
+                            <IonCheckbox checked={this.state.new_game.type=="input"} onIonChange={e => this.setRiddleChecked(e.detail.checked)} />
+                        </IonItem>
+                        {this.state.new_game.type=="input" &&
+                        <IonItem>
+                            <IonTextarea placeholder="Texte de la question / devinette"  onIonChange={e => this.setText("riddle_question",e.detail.value!)}></IonTextarea>
+                        </IonItem>}
+                        {this.state.new_game.type=="input" &&
+                        <IonItem>
+                            <IonInput placeholder="Réponse à la question / devinette" onIonChange={e => this.setText("riddle_answer",e.detail.value!)}></IonInput>
+                        </IonItem>
+                        }
 
                     </IonList>
 
-                    <div style={{"alignContent":"center", "marginTop":"5px"}}>
+                    <div style={{"alignContent":"center", "marginLeft":'10px'}}>
 
                         <IonButton color="success" onClick={() => this.setShowModal(false, true)} >Sauvegarder</IonButton>
                         <IonButton color="danger" onClick={() => this.setShowModal(false, false)} >Réinitialiser</IonButton>
@@ -153,7 +244,7 @@ class MissionComponent extends Component<MapProps> {
                     <div>
                         <IonHeader>
                         <IonToolbar>
-                          <IonTitle >Toutes les missions</IonTitle>
+                          <IonTitle >Missions pré-enregistrées</IonTitle>
                         </IonToolbar>
                       </IonHeader>
 
